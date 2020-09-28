62c164c3a432838884abd46feef6ab9af8c3332f
Sara T <sahara.tijol@gmail.com>
2020-09-28 13:57:52 -0500

    Finished Intro to Java Exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, IO!");
+        System.out.print("Hello,");
+        System.out.print(" Bio!");
+        System.out.println(); //puts a newline
+    }
+}
+
+
