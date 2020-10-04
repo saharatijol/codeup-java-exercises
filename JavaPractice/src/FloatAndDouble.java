2765fbef53ab12872bf3c862da556716f1e7174b
Sara T <sahara.tijol@gmail.com>
2020-10-03 20:33:13 -0500

    Basic Java Exercises

---
+++ JavaPractice/src/FloatAndDouble.java
@@ -0,0 +1,11 @@
+public class FloatAndDouble {
+    public static void main (String[] args) {
+
+        // Convert a given number of pounds to kilograms.
+
+        double pounds = 200d;
+        double poundToKg = pounds * 0.45359237;
+
+        System.out.println(poundToKg + " kilograms");
+    }
+}
