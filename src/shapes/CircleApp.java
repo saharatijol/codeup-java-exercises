953a4d8abac9bf46c2e5b0eedcacbc74675d7cd0
Sara T <sahara.tijol@gmail.com>
2020-10-06 14:27:22 -0500

    OOP Exercise complete. Some BONUS done

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -8,14 +8,15 @@ public class CircleApp {
         boolean again = false;
 
         do {
+
             Input radius = new Input();
             //System.out.println("Radius of circle: ");
-
             double enterRadius = radius.getDouble("Enter Radius for circle: ");
             Circle displayCircle = new Circle(enterRadius);
 
             double area = displayCircle.getArea();
             double circumference = displayCircle.getCircumference();
+
             System.out.println("Radius: " + enterRadius);
             System.out.println("Area: " + area);
             System.out.println("Circumference: " + circumference);
--- src/util/Input.java
+++ src/util/Input.java
@@ -11,29 +11,37 @@ public class Input {
     public String getString() {
         System.out.println("Type a string: ");
         String typedString = scanner.nextLine();
+
         return typedString;
     }
 
     public boolean yesNo() {
         System.out.println("yey or nay?: ");
         String choice = scanner.nextLine();
+
         if (choice.equalsIgnoreCase("yey") || choice.startsWith("y")) {
             return true;
+
         } else if (choice.equalsIgnoreCase("nay") || choice.startsWith("n")) {
             return false;
+
         } else {
             System.out.println("What is that? That's not an option.. Again");
             return yesNo();
         }
     }
 
+    // Optional String prompt
     public boolean yesNo(String prompt) {
         System.out.println(prompt);
         String choice = scanner.nextLine();
+
         if (choice.equalsIgnoreCase("yes") || choice.equalsIgnoreCase("y")) {
             return true;
+
         } else if (choice.equalsIgnoreCase("no") || choice.equalsIgnoreCase("n")) {
             return false;
+
         } else {
             System.out.println("What is that? That's not an option.. Again");
             return yesNo(prompt);
@@ -43,12 +51,15 @@ public class Input {
     public int getInt(int min, int max) {
         System.out.printf("Enter number between %d and %d: ", min, max);
         int guessNum = parseInt(scanner.nextLine());
+
         if (guessNum > max) {
             System.out.println("You're above range.. Again ");
             return getInt(min, max);
+
         } else if (guessNum < min) {
             System.out.println("You're below range.. Again");
             return getInt(min, max);
+
         } else {
             System.out.println("You are within range.. Next");
             return guessNum;
@@ -64,14 +75,17 @@ public class Input {
     public double getDouble(double min, double max) {
         System.out.printf("Enter number between %s and %s: ", min, max);
         double guessDouble = parseDouble(scanner.nextLine());
+
         if (guessDouble > max) {
             System.out.println("You're ABOVE range.. Again ");
             return getDouble(min, max);
+
         } else if (guessDouble < min) {
             System.out.println("You're BELOW range.. Again");
             return getDouble(min, max);
+
         } else {
-            System.out.println("You WITHIN range... Next");
+            System.out.println("You're WITHIN range... Next");
             return guessDouble;
         }
 
@@ -83,20 +97,12 @@ public class Input {
         return numDouble;
     }
 
+    // Optional String prompt
     public double getDouble(String prompt) {
         System.out.println(prompt);
         double numDouble = parseDouble(scanner.nextLine());
+
         return numDouble;
     }
 
-//    public static void main(String[] args) {
-//        Input input1 = new Input ();
-//        System.out.println(input1.getString());
-//        System.out.println(input1.yesNo());
-//        System.out.println(input1.getInt(1, 10));
-//        System.out.println(input1.getInt());
-//        System.out.println(input1.getDouble(1.0, 10.5));
-//        System.out.println(input1.getDouble());
-//
-//    }
 }
