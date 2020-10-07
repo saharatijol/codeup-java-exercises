ff079c60478988d481b2c3b8d2445e6b903e7122
Sara T <sahara.tijol@gmail.com>
2020-10-07 09:54:02 -0500

    Arrays lecture notes1

---
+++ src/ArraysLec.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class ArraysLec {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Fer", "Douglas", "Daniel"};
+        double[] prices = new double[3]; // *you can't change the size of an array once it is created
+        int [] numbers = new int[8];
+        int [] otherNumbers = new int[8];
+
+        prices[0] = Double.parseDouble("1.0");
+        prices[1] = 2.0 * 2;
+        prices[2] = 3.0;
+
+//        System.out.println(names[0]);
+//        System.out.println(names[2]);
+
+        // System.out.println("prices = " + prices); // ** Only prints its spot in memory **
+
+        System.out.println("prices = " + prices.length + " length");
+
+//        System.out.println("prices[0] = " + prices[0]);
+//        System.out.println("prices[2] = " + prices[2]);
+
+        for (int i = 0; i < prices.length; i++) {
+            System.out.println("price[i] = " + prices[i]);
+        }
+
+
+        for (String name: names) {
+            System.out.println("name = " + name);
+        }
+
+        // ** Most commonly used Arrays.copyOf(array, length) and Arrays.equals(array1, array2)
+        Arrays.sort(prices);
+        Arrays.sort(names);
+
+        System.out.println("Arrays.names = " + Arrays.toString(names));
+        System.out.println("Arrays.price = " + Arrays.toString(prices));
+
+        Arrays.fill(numbers, 10);
+        Arrays.fill(otherNumbers, 5);
+
+        System.out.println("Arrays.equals(otherNumbers, otherNumbers) = " + Arrays.equals(otherNumbers, otherNumbers));
+        System.out.println("Arrays.equals(numbers, otherNumbers) = " + Arrays.equals(numbers, otherNumbers));
+
+        System.out.println("Arrays.toString() =  " + Arrays.toString(numbers));
+
+        //String[] copyOfNames = new String[names.length + 1]; // You have to bring each item manually
+        String[] copyOfNames = Arrays.copyOf(names, names.length + 1);
+
+        System.out.println("copyOfNames = " + copyOfNames.length);
+        copyOfNames[3] = "Justin";
+        System.out.println("Arays.toString(copyOfNames) = " + Arrays.toString(copyOfNames));
+
+    }
+}
