c7fc3d82c8908dae71f6d294ea463c60a12fa7b1
Sara T <sahara.tijol@gmail.com>
2020-10-07 15:16:26 -0500

    Solved sumOdd exercise

---
+++ JavaPractice/ControlFlowStatements/src/SumOddRange.java
@@ -0,0 +1,34 @@
+public class SumOddRange {
+
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(13, 13));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, 1000));
+    }
+
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        } else return number % 2 == 1;
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start < 0 || end < 0) {
+            return -1;
+        } else if (end < start) {
+            return -1;
+        } else {
+            int sum = 0;
+            for (int num = start; num <= end; num++) {
+            //System.out.println(num + " = " + isOdd(num));
+                if(isOdd(num)) {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+    }
+}
