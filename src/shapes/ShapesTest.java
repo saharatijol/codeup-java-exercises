623041f647fddd7fa1174796b118f214fe8e2fcd
Sara T <sahara.tijol@gmail.com>
2020-10-08 13:38:38 -0500

    Shapes exercise complete

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,25 @@
+package shapes;
+
+public class ShapesTest {
+
+    public static void main(String[] args) {
+
+        // BOX 1
+        Rectangle box1 = new Rectangle (4, 5);
+        System.out.println("---- BOX 1 ----");
+        System.out.println("getPerimeter() = " + box1.getPerimeter());
+        System.out.println("getArea() = " + box1.getArea());
+
+        // BOX 2
+        Rectangle box2 = new Square (5);
+        System.out.println("---- BOX 2 ----");
+        System.out.println("getPerimeter() = " + box2.getPerimeter());
+        System.out.println("getArea() = " + box2.getArea());
+
+        // BOX 3 Method Overriding
+        Rectangle box3 = new Square(5);
+        System.out.println("---- BOX 3 ----");
+        System.out.println("getPerimeter() = " + box3.getPerimeter());
+        System.out.println("getArea() = " + box3.getArea());
+    }
+}
