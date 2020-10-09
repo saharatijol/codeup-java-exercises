6e62ea2eb3ea9167b597219b3233aa81ff5ff678
Sara T <sahara.tijol@gmail.com>
2020-10-09 09:54:18 -0500

    Added notes on exercis after walk through

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,21 @@
 package shapes;
 
 public class Square extends Rectangle {
-    private double side;
+    private double side; // remove this cause it not needed
 
     // Constructor
     public Square (double side) {
         super(side, side);
-        this.side = side;
+        this.side = side; // remove, not needed
     }
 
     public double getArea() {
         return 4 * this.side;
+        // return 4 * this.width
     }
 
     public double getPerimeter() {
         return this.side * this.side;
+        // return this.length * this.length to take
     }
 }
