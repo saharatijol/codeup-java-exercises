ea7c2f7bcd009571240403555f2751d155121d49
Sara T <sahara.tijol@gmail.com>
2020-10-12 01:19:21 -0500

    LastDigitChecker solved

--- JavaPractice/Loops/src/LastDigitChecker.java
+++ JavaPractice/Loops/src/LastDigitChecker.java
@@ -1,7 +1,31 @@
 public class LastDigitChecker {
 
-    /*
-    * Write a method named hasSameLastDigit with three parameters of type int
-    * Each number should be within the range of 10
-    * */
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+
+        System.out.println(isValid(10));
+        System.out.println(isValid(468));
+        System.out.println(isValid(1051));
+    }
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+
+        if ((num1 < 10 || num1 > 1000) || (num2 < 10 || num2 > 1000) || (num3 < 10 || num3 > 1000)) {
+            return false;
+        } else {
+            int lastDigit1 = num1 % 10;
+            int lastDigit2 = num2 % 10;
+            int lastDigit3 = num3 % 10;
+            if (lastDigit1 == lastDigit2 || lastDigit2 == lastDigit3 || lastDigit3 == lastDigit1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isValid(int num) {
+        return num >= 10 && num <= 1000;
+    }
 }
