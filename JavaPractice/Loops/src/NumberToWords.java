f835070cec9ab95f80edb5ca8c7ad4ed8bdc0225
Sara T <sahara.tijol@gmail.com>
2020-10-12 09:14:01 -0500

    Number to words conversion

---
+++ JavaPractice/Loops/src/NumberToWords.java
@@ -0,0 +1,13 @@
+public class NumberToWords {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void numberToWords (int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+    }
+}
