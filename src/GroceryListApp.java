eceff3807eda2c0468c65f09dc0f2c60f0679b5e
Sara T <sahara.tijol@gmail.com>
2020-10-13 16:02:16 -0500

    Built hashMap for categories and items within them

--- src/GroceryListApp.java
+++ src/GroceryListApp.java
@@ -16,6 +16,38 @@ public class GroceryListApp {
         produce.addItem("apples");
         groceryItems.put("Produce" ,produce);
 
+        GroceryList meats = new GroceryList("Meat");
+        meats.addItem("chicken");
+        meats.addItem("beef");
+        meats.addItem("turkey");
+        meats.addItem("lamb");
+        meats.addItem("pork");
+        groceryItems.put("Meats" ,meats);
+
+        GroceryList personalCare = new GroceryList("Personal Care");
+        personalCare.addItem("shampoo");
+        personalCare.addItem("soap");
+        personalCare.addItem("lotion");
+        personalCare.addItem("toothpaste");
+        personalCare.addItem("deodorant");
+        groceryItems.put("Personal Care" ,personalCare);
+
+        GroceryList cleaners = new GroceryList("Cleaners");
+        cleaners.addItem("detergent");
+        cleaners.addItem("dishwashing soap");
+        cleaners.addItem("floor solution");
+        cleaners.addItem("sponge");
+        cleaners.addItem("windex");
+        groceryItems.put("Cleaners" ,cleaners);
+
+        GroceryList beverages = new GroceryList("Beverages");
+        beverages.addItem("soda pop");
+        beverages.addItem("orange juice");
+        beverages.addItem("coffee");
+        beverages.addItem("tea");
+        beverages.addItem("water");
+        groceryItems.put("Beverages" ,beverages);
+
 
         Input userInp = new Input();
 
@@ -27,6 +59,7 @@ public class GroceryListApp {
             System.out.println("Alright then... Next time..");
         } else {
             categoryUI();
+
             int userCategory = userInp.getInt("Select one (1-5): ");
 
             if(userCategory == 1) {
