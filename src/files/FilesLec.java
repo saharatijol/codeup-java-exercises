2f54c8067177d34ba828d6fe16c5bcab1296bd2d
Sara T <sahara.tijol@gmail.com>
2020-10-20 11:07:43 -0500

    FilesIO update

--- src/files/FilesLec.java
+++ src/files/FilesLec.java
@@ -1,5 +1,7 @@
 package files;
 
+import util.Input;
+
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.nio.file.Files;
@@ -15,15 +17,7 @@ public class FilesLec {
 
         String directoryName = "data";
         String fileName = "grocery_list.txt";
-
-        // This will create grocery_list.txt in the project folder directly
-        //Path dataFilePath = Paths.get(fileName);
-
-        // OUTPUT the path - DON'T DO THIS
-        // dataFilePath.getFileName();
-
-        // Files.createFile(dataFilePath);
-        // This will break bec Java does not know we are creating a file, it does not recognize it
+        Input input = new Input();
 
         // We have to create a directory or folder first BEFORE we get to create our file
         try { // Try to catch exception if we are out of disk space
@@ -34,36 +28,32 @@ public class FilesLec {
             Files.write(dataFilePath, groceryList);
 
             // Let's read our file
-//            List<String> fileContents = Files.readAllLines(dataFilePath);
-//            for (int i = 0; i < fileContents.size(); i++) {
-//                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
-//            }
             // Now, let's refactor the loop above
             FileIO.printFileContents(dataFilePath);
 
             // Append to the file. StandardOpenOption.APPEND will prevent overwriting
-            Files.write(dataFilePath, Arrays.asList("cereal", "bread"), StandardOpenOption.APPEND);
-            FileIO.printFileContents(dataFilePath);
+            //String userInp = input.getString();
+//            Files.write(dataFilePath, Arrays.asList("cereal", "bread"), StandardOpenOption.APPEND);
+//            FileIO.printFileContents(dataFilePath);
 
             // To edit our file
             // 1. Read a file in.
             // 2. then, replace a line on file
-            FileIO.updateLine(dataFilePath, "milks", "whole milk");
-            FileIO.printFileContents(dataFilePath);
+//            FileIO.updateLine(dataFilePath, "milks", "whole milk");
+//            FileIO.printFileContents(dataFilePath);
 
             // Remove a line from the file
-            FileIO.deleteLine(dataFilePath, "bread");
-            FileIO.printFileContents(dataFilePath);
+//            FileIO.deleteLine(dataFilePath, "bread");
+//            FileIO.printFileContents(dataFilePath);
 
             // To Empty List
-            Files.write(dataFilePath, new ArrayList<>());
-            System.out.println("After empty");
-            FileIO.printFileContents(dataFilePath);
+//            Files.write(dataFilePath, new ArrayList<>());
+//            System.out.println("After empty");
+//            FileIO.printFileContents(dataFilePath);
 
         } catch (IOException ex) {
             System.out.println("Cannot create file");
             ex.printStackTrace();
         }
-
     }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -119,5 +119,4 @@ public class Input {
 
         return numDouble;
     }
-
 }
