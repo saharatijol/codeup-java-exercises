2fc18595e17fe6f2bc02761e62b0a31446d4fc14
Sara T <sahara.tijol@gmail.com>
2020-11-09 23:45:53 -0600

    Created setters, will have to check if wrote correctly

--- JavaPractice/OOPBasics1/src/BankAccount.java
+++ JavaPractice/OOPBasics1/src/BankAccount.java
@@ -20,12 +20,27 @@ public class BankAccount {
     public String getEmail() {
         return this.email;
     }
-
     public long getPhone_no() {
         return this.phone_no;
     }
 
     // Setters
+    public void setAccount_no(long account_no) {
+        this.account_no = account_no;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhone_no(long phone_no) {
+        this.phone_no = phone_no;
+    }
+
     // Create 2 additional methods
     // 1. allow customer to deposit funds
     // 2. allow customer to withdraw
