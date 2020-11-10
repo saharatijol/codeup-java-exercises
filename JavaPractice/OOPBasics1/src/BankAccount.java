932e41cbeed4e272ffd5d29657d2831c99ae3005
Sara T <sahara.tijol@gmail.com>
2020-11-09 23:40:53 -0600

    Created getters for fields

--- JavaPractice/OOPBasics1/src/BankAccount.java
+++ JavaPractice/OOPBasics1/src/BankAccount.java
@@ -1,5 +1,5 @@
 public class BankAccount {
-    // Fields: account number, balance, customer name, email and phone number
+
     private long account_no;
     private double balance;
     private String customer_name;
@@ -7,7 +7,25 @@ public class BankAccount {
     private long phone_no;
 
     // Create getters and setters for each field
+    // Getters
+    public long getAccount_no() {
+        return this.account_no;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+    public String getCustomer_name() {
+        return this.customer_name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
 
+    public long getPhone_no() {
+        return this.phone_no;
+    }
+
+    // Setters
     // Create 2 additional methods
     // 1. allow customer to deposit funds
     // 2. allow customer to withdraw
