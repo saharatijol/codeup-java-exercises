de272ad9a57260c9033b2bed94145b08a624e7a1
Sara T <sahara.tijol@gmail.com>
2020-11-11 20:05:09 -0600

    Start on Carpet Cost Calculator

---
+++ JavaPractice/OOPBasics1/src/Floor.java
@@ -0,0 +1,15 @@
+
+public class Floor {
+
+    private double width = 0;
+    private double length = 0;
+
+    public Floor (double length, double width) {
+        if(length > 0) {
+            this.length = length;
+        }
+        if (width > 0) {
+            this.width = width;
+        }
+    }
+}
