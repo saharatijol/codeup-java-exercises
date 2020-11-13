c5483a173f9d15d910693cba3d34eee85f4ef1df
Sara T <sahara.tijol@gmail.com>
2020-11-12 22:28:57 -0600

    Complex number problem solved

--- JavaPractice/OOPBasics1/src/ComplexNumber.java
+++ JavaPractice/OOPBasics1/src/ComplexNumber.java
@@ -24,6 +24,17 @@ public class ComplexNumber {
         add(num.getReal(), num.getImaginary());
     }
 
+    public void subtract (double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber num) {
+        subtract(num.getReal(), num.getImaginary());
+    }
+
+
+
 
     public static void main(String[] args) {
         ComplexNumber one = new ComplexNumber(1.0, 1.0);
@@ -31,6 +42,14 @@ public class ComplexNumber {
         one.add(1,1);
         System.out.println("one.getReal() = " + one.getReal());
         System.out.println("one.getImaginary() = " + one.getImaginary());
+
+        one.subtract(number);
+        System.out.println("one.getReal() = " + one.getReal());
+        System.out.println("one.getImaginary() = " + one.getImaginary());
+
+        number.subtract(one);
+        System.out.println("number.getReal() = " + number.getReal());
+        System.out.println("number.getImaginary() = " + number.getImaginary());
     }
 
 
