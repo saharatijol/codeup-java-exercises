76fcd176dfa41ed087eaf3da189bc6731e379145
Sara T <sahara.tijol@gmail.com>
2020-11-12 22:24:35 -0600

    Create and test add methods for complex numbers

--- JavaPractice/OOPBasics1/src/ComplexNumber.java
+++ JavaPractice/OOPBasics1/src/ComplexNumber.java
@@ -16,7 +16,21 @@ public class ComplexNumber {
     }
 
     public void add (double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
 
+    public void add(ComplexNumber num) {
+        add(num.getReal(), num.getImaginary());
+    }
+
+
+    public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.getReal() = " + one.getReal());
+        System.out.println("one.getImaginary() = " + one.getImaginary());
     }
 
 
