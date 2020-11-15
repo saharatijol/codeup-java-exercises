b7bb69f976c0d85df4529c2d59b34d9b999b09a6
Sara T <sahara.tijol@gmail.com>
2020-11-15 01:11:50 -0600

    Easy level inheritance exercise

--- JavaPractice/OOPBasics1/src/Main.java
+++ JavaPractice/OOPBasics1/src/Main.java
@@ -61,6 +61,14 @@ public class Main {
 //        outlander.accelerate(20);
 //        outlander.accelerate(-42);
 
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.getRadius() = " + circle.getRadius());
+        System.out.println("circle.getArea() = " + circle.getArea());
 
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.getRadius() = " + cylinder.getRadius());
+        System.out.println("cylinder.getHeight() = " + cylinder.getHeight());
+        System.out.println("cylinder.getArea() = " + cylinder.getArea());
+        System.out.println("cylinder.getVolume() = " + cylinder.getVolume());
     }
 }
